1ad4f84b077e516b693a66e9d419e6ec476cfe0c
DanielDuong1998 <dvkhangnt@gmail.com>
2020-07-02 13:19:49 +0700

    add api get info stk of partner bank

--- routes/money_partner_group2.route.js
+++ routes/money_partner_group2.route.js
@@ -22,6 +22,7 @@ router.post('/info', async(req, res)=>{
 	// }
 
 	const { secretString, desBankCode, urlInfo } = config.foreignBank.group2;
+	const { algorithm, expiresIn } = config.foreignBank.group2.jwt;
 	const desAccountNumber = req.body.stk_thanh_toan;
 
 	const payload = {
@@ -30,17 +31,70 @@ router.post('/info', async(req, res)=>{
 		iat: getIssuedAtNow()
 	}
 
-	const x_hashed_data = await jwt.sign
-		
-	res.json({
-		msg: 'post method route money partner group 2' 
+	const x_hashed_data = await jwt.sign({payload}, secretString, {algorithm, expiresIn});
+
+	const headers = ({
+		x_hashed_data
 	});
+
+	const data = {
+		desAccountNumber,
+		desBankCode,
+		iat: getIssuedAtNow()
+	}
+
+	const dataString = JSON.stringify(data);
+	const encrypted_data = await encrypted_dataF(dataString);
+	const body = ({
+		encrypted_data
+	});
+
+	const options = {
+		url: urlInfo,
+		headers,
+		method: 'POST',
+		body,
+		json: true
+	};
+
+	const callback = (err, response, body)=>{
+		if(err) throw err;
+		console.log('body: ', body);
+		res.json(body);
+	}
+
+	request(options, callback);
+
+		
+	// res.json({
+	// 	msg: 'post method route money partner group 2' 
+	// });
 });
+
+router.post('/add-money', async(req, res)=>{
+
+});
+
 
 
 
 const getIssuedAtNow = _=>{
 	return momentTz.tz('Asia/Bangkok').unix();
+}
+
+const encrypted_dataF = async  dataString=> {
+    await openpgp.initWorker();
+
+    const publicKeyArmored = key.pubKey(0);
+
+    const { data: encrypted } = await openpgp.encrypt({
+        message: openpgp.message.fromText(dataString), // input as Message object
+        publicKeys: (await openpgp.key.readArmored(publicKeyArmored)).keys, // for encryption
+    });
+
+    openpgp.destroyWorker();
+
+    return encrypted;
 }
 
 
